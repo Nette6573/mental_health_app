cbc2d410b111a58be943e0e5895b9ef4c8b6317b
Brennanlondon <127898339+Brennanlondon@users.noreply.github.com>
2026-04-04 15:45:50 -0500

    Update page.tsx

--- frontend/src/app/provider-dashboard/settings/page.tsx
+++ frontend/src/app/provider-dashboard/settings/page.tsx
@@ -14,7 +14,7 @@ import {
   sendEmailVerification
 } from 'firebase/auth'
 import { doc, setDoc, getDoc } from 'firebase/firestore'
-import { auth, db, googleProvider, facebookProvider } from "../lib/firebaseClient";
+import { auth, db, googleProvider, facebookProvider } from "@/lib/firebaseClient";
 
 
 const AuthContext = createContext(null)
