11fa8973cb687a22733717afd1ec03731e3b42ff
Brennanlondon <127898339+Brennanlondon@users.noreply.github.com>
2026-04-14 20:27:17 -0500

    Update layout.js

--- frontend/src/app/provider-dashboard/layout.js
+++ frontend/src/app/provider-dashboard/layout.js
@@ -4,7 +4,7 @@ import ProtectedRoute from "@/components/auth/ProtectedRoute"
 
 export default function ProviderLayout({ children }) {
   return (
-    <ProtectedRoute allowedRole="provider">
+    //<ProtectedRoute allowedRole="provider">
       {children}
     </ProtectedRoute>
   )
